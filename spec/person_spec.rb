ed73accd79b6e364a5654558fbb166a8da558923
mohamedabdimohammud <mmohammud@outlook.com>
2023-05-23 09:37:38 +0300

    Completed assignment

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -321,3 +321,82 @@ RSpec.describe 'Person' do
     end
   end
 end
+class Person
+  attr_reader :name, :bank_account, :happiness, :hygiene
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def bank_account=(amount)
+    @bank_account = amount
+  end
+
+  def happiness=(value)
+    @happiness = limit_value(value, 0, 10)
+  end
+
+  def hygiene=(value)
+    @hygiene = limit_value(value, 0, 10)
+  end
+
+  def happy?
+    happiness > 7
+  end
+
+  def clean?
+    hygiene > 7
+  end
+
+  def get_paid(amount)
+    self.bank_account += amount
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    case topic
+    when "politics"
+      self.happiness -= 2
+      person.happiness -= 2
+      "blah blah partisan blah lobbyist"
+    when "weather"
+      self.happiness += 1
+      person.happiness += 1
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
+  end
+
+  private
+
+  def limit_value(value, min, max)
+    if value < min
+      min
+    elsif value > max
+      max
+    else
+      value
+    end
+  end
+end
